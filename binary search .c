f2055ebf01a1ea7d260775e37fc83a66fa2a990e
sudiptadey877 <117027964+sudiptadey877@users.noreply.github.com>
2022-10-30 21:20:17 +0530

    Create binary search .c

---
+++ binary search .c
@@ -0,0 +1,35 @@
+// Binary Search in C
+
+#include <stdio.h>
+
+int binarySearch(int array[], int x, int low, int high) {
+  // Repeat until the pointers low and high meet each other
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main() {
+  int array[] = {3, 4, 5, 6, 7, 10, 9};
+  int n = sizeof(array) / sizeof(array[0]);
+  int x;
+  printf("Enter data that u want to search:");
+  scanf("%d",&x);
+  int result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+  return 0;
+}
